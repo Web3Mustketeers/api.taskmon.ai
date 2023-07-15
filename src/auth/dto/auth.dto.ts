693126227a9b2cc4997fb0d49cceac6f6f264b34
Hiro <laciferin@gmail.com>
2023-07-15 11:51:02 +0530

    wip: isSolanaAddress

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,16 +1,28 @@
-import { IsEmail, IsEthereumAddress, IsNotEmpty, IsStrongPassword } from 'class-validator'
-// import {PublicKey} from '@solana/web3.js'
-//
-// function validateSolAddress(address:string){
-//   try {
-//     let pubkey = new PublicKey(address)
-//     let  isSolana =  PublicKey.isOnCurve(pubkey.toBuffer())
-//     return isSolana
-//   } catch (error) {
-//     return false
-//   }
+import { IsNotEmpty } from 'class-validator'
+// import { PublicKey } from '@solana/web3.js'
+/*
+function IsSolanaAddress(address:string){
+  try {
+    let pubkey = new PublicKey(address)
+    let  isSolana =  PublicKey.isOnCurve(pubkey.toBuffer())
+    return isSolana
+  } catch (error) {
+    return false
+  }
 export class AuthDto {
   // @IsEthereumAddress()
+  @IsSolanaAddress()
+  @IsNotEmpty()
+  wallet: string
+}
+*/
+
+// function IsSolanaAddress(): PropertyDecorator {
+//   return
+// }
+
+export class AuthDto {
+  // @IsSolanaAddress()
   @IsNotEmpty()
   wallet: string
 }
