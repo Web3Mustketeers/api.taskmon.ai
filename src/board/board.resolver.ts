752200ded92ab6c20ad2622aeaf16bad72ace53a
Hiro <laciferin@gmail.com>
2023-07-16 13:24:34 +0530

    fix: board resolver
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 2 minutes

--- src/board/board.resolver.ts
+++ src/board/board.resolver.ts
@@ -1,29 +1,34 @@
-import { Mutation, Query, Resolver } from "@nestjs/graphql";
-import { BoardService } from "./board.service";
-import { CreateBoardInput } from "./dto/create-board.input";
-import { UpdateBoardInput } from "./dto/update-board.input";
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
+import { BoardService } from './board.service'
+import { CreateBoardInput } from './dto/create-board.input'
+import { UpdateBoardInput } from './dto/update-board.input'
 
 @Resolver('Board')
 export class BoardResolver {
   constructor(private readonly boardService: BoardService) {}
 
-  @Query('b"boards"  findAll() {
-    return this.boardService.findAll()
- ; }
-
-  @Query('b"board"  findOne(@Args('i"id"id: number) {
-    return this.boardService.findOne({ id })
- ; }
-
-  @Mutation('c"createBoard"  create(@Args('c"createBoardInput"createBoardInput: CreateBoardInput) {
+  @Mutation('createBoard')
+  create(@Args('createBoardInput') createBoardInput: CreateBoardInput) {
     return this.boardService.create(createBoardInput)
- ; }
+  }
 
-  @Mutation('u"updateBoard"  update(@Args('u"updateBoardInput"updateBoardInput: UpdateBoardInput) {
+  @Query('boards')
+  findAll() {
+    return this.boardService.findAll()
+  }
+
+  @Query('board')
+  findOne(@Args('id') id: number) {
+    return this.boardService.findOne({ id })
+  }
+
+  @Mutation('updateBoard')
+  update(@Args('updateBoardInput') updateBoardInput: UpdateBoardInput) {
     return this.boardService.update(updateBoardInput.id, updateBoardInput)
- ; }
+  }
 
-  @Mutation('r"removeBoard"  remove(@Args('i"id"id: number) {
+  @Mutation('removeBoard')
+  remove(@Args('id') id: number) {
     return this.boardService.remove(id)
- ; }
+  }
 }
