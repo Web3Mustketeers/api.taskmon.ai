222acb8988fee451127a88c13108a05f30a05170
Hiro <laciferin@gmail.com>
2023-07-27 19:40:09 +0530

    wip: authResolver
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 7 minutes

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,10 +1,19 @@
-import { Mutation, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Resolver } from '@nestjs/graphql'
 import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+import { Res } from '@nestjs/common'
+import { Response } from 'express'
 
 @Resolver()
 export class AuthResolver {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly authController: AuthController,
+  ) {}
 
   @Mutation('signIn')
-  signIn() {}
+  signIn(@Args('wallet') wallet: string, @Res() res: Response) {
+    // return this.authService.signin({ wallet }) //THis works
+    return this.authController.signin({ wallet }, res)
+  }
 }
