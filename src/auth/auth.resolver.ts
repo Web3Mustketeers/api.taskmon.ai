6762a2cc5ce2c91c07d03a91d6e0781a878c554b
Hiro <laciferin@gmail.com>
2023-07-28 14:23:48 +0530

    doc: http
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 24 minutes

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,7 +1,11 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql'
+import {
+  Args,
+  Context,
+  GraphQLExecutionContext,
+  Mutation,
+  Resolver,
+} from '@nestjs/graphql'
 import { AuthService } from './auth.service'
-import { Res } from '@nestjs/common'
-import { Response } from 'express'
 import { AuthController } from './auth.controller'
 
 @Resolver('Auth')
@@ -12,10 +16,15 @@ export class AuthResolver {
   ) {}
 
   @Mutation('signIn')
-  signIn(@Args('wallet') wallet: string, @Res() res: Response) {
-    console.debug({ res })
+  signIn(@Args('wallet') wallet: string, @Context() ctx: GraphQLExecutionContext) {
+    console.debug({ ctx })
     console.info({ wallet })
-    return this.authService.signin({ wallet }) //FIXME:
-    // return this.authController.signin({ wallet }, res)
+    // return this.authService.signin({ wallet }) //FIXME:
+    const context = ctx.getContext()
+
+    context.res.cookie('some-cookie', 'some-value')
+    context.res.header('some-header', 'some-header')
+
+    return this.authController.signin({ wallet }, context.res)
   }
 }
