f8c8c022ad1cf1b50ddd4a9969d97a6c77b84f48
Hiro <laciferin@gmail.com>
2023-07-28 07:14:19 +0530

    wip: fixes authService graphql
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 1 minute

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,21 +1,19 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql'
 import { AuthService } from './auth.service'
-import { AuthController } from './auth.controller'
 import { Res } from '@nestjs/common'
 import { Response } from 'express'
 
 @Resolver('Auth')
 export class AuthResolver {
   constructor(
-    private readonly authService: AuthService, //FIXME: remove
-    private readonly authController: AuthController,
+    private readonly authService: AuthService, //FIXME: remove // private readonly authController: AuthController,
   ) {}
 
   @Mutation('signIn')
   signIn(@Args('wallet') wallet: string, @Res() res: Response) {
     console.debug({ res })
     console.info({ wallet })
-    // return this.authService.signin({ wallet }) //FIXME:
+    return this.authService.signin({ wallet }) //FIXME:
     // return this.authController.signin({ wallet }, res)
   }
 }
