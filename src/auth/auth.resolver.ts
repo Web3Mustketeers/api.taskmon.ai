4577dcd26c7486b864904e996dd0cee7ec3a90fd
Hiro <laciferin@gmail.com>
2023-07-30 06:58:01 +0530

    cookie: fixed
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 3 minutes

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -5,30 +5,17 @@ import { AuthController } from './auth.controller'
 @Resolver('Auth')
 export class AuthResolver {
   constructor(
-    private readonly authService: AuthService, //FIXME: remove
+    private readonly authService: AuthService,
     private readonly authController: AuthController,
   ) {}
 
   @Mutation('signIn')
   async signIn(@Args('wallet') wallet: string, @Context() ctx: any) {
-    // const { res } = ctx
-    /*  res.cookie('cookieName', 'cookieValue', {
-      // ...Cookie options
-    })*/
-
     console.info({ wallet })
-    // return this.authService.signin({ wallet }) //FIXME:
-    const context = ctx.getContext()
 
-    context.res.cookie('some-cookie', 'some-value')
-    context.res.header('some-header', 'some-header')
+    // ctx.res.cookie('some-cookie', 'some-value')
+    // ctx.res.header('some-header', 'some-header')
 
-    // ctx.switchToHttp().getResponse<Response>().cookie('some-cookie', 'some-value')
-    // ctx.switchToHttp().getResponse<Response>().header('some-header', 'some-header')
-
-    const dto = await this.authService.signin({ wallet })
-
-    // return this.authController.signin({ dto })
-    return dto
+    return this.authController.signin({ wallet }, ctx.res)
   }
 }
